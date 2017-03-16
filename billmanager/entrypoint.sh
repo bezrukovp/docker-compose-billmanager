431e6a24fc86f955cbc57d8014bbe30823c9b829
Pavel Bezrukov <bezrukov.ps@gmail.com>
2017-03-17 00:10:55 +0300

    Initial commit

---
+++ billmanager/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+[ -z "${TERM}" ] && export TERM=xterm
+
+func_TERM() {
+    service cron stop
+    service ihttpd stop
+    service mysql stop
+    exit 0
+}
+
+trap func_TERM TERM INT
+
+case $1 in
+
+    start|billmgr)
+	service mysql start
+	service cron start
+	service ihttpd start
+
+	shift
+	exec $0 daemon
+    ;;
+    daemon)
+	while true; do
+	    sleep 10 &
+	    wait $!
+	done
+    ;;
+    *)
+	exec $@
+    ;;
+esac
